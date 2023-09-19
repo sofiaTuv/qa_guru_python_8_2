603b7e0e413ddfac474d2e47d8a8ef25b7f3b5f5
Sofia Tuvykina <hdfaa.56@gmail.com>
2023-09-19 12:57:43 +0300

    Добавлено два теста

--- test_google_should_file.py
+++ test_google_should_file.py
@@ -2,13 +2,13 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-def test_successful(open_browser, size_browser):
-    browser.open('https://google.com')
+def test_successful():
+    browser.open("https://www.google.com/")
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
 
-def test_not_successful(open_browser, size_browser):
-    browser.open('https://google.com')
+def test_not_successful():
+    browser.open("https://www.google.com/")
     browser.element('[name="q"]').should(be.blank).type('dgdfdgdhdfgfg').press_enter()
-    browser.element('[class="card-section"]').should(have.text('По запросу  ничего не найдено.'))
+    browser.element('[class="card-section"]').should(have.text('По запросу dgdfdgdhdfgfg ничего не найдено.'))
